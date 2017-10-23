d3b7721466bd155158e6cdeda051b745bfe6bef3
Timothy William Padjen <twpadjen@dcsdk12.org>
2017-10-23 07:59:48 -0600

    Refactor iterate to use a for each loop

--- iteration.py
+++ iteration.py
@@ -3,6 +3,12 @@
 # [1, 5, 7 ,8 , 4, 3]
 
 def iterate(list):
-	for i in range(0, len(list)):
-		print list[i]
+	# standard for loop with range
+	# for i in range(0, len(list)):
+	# 	print list[i]
+
+	# for each loop
+	for item in list:
+		print item
+
 
