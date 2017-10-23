9b3d033a6753b9357a35ee795c43d05363446002
Timothy William Padjen <twpadjen@dcsdk12.org>
2017-10-23 07:55:01 -0600

    Print a list

---
+++ iteration.py
@@ -0,0 +1,8 @@
+# iteration pattern
+
+# [1, 5, 7 ,8 , 4, 3]
+
+def iterate(list):
+	for i in range(0, len(list)):
+		print list[i]
+
---
+++ test.py
@@ -0,0 +1,9 @@
+from iteration import *
+
+scores = [100, 90, 85, 100, 0, 86, 50, 100]
+
+iterate(scores)
+
+names = ["Amy", "Bill", "Carol", "Dan", "Emily"]
+
+iterate(names)
